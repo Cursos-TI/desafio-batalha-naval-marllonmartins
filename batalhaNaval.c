5631353db75681b8ee881e8f5da5d525bff9b9c9
marllonmartins <paplays40@gmail.com>
2025-06-10 21:27:07 +0000

    Nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,96 +1,93 @@
 #include <stdio.h>
 
-#define N 10
-#define TAM_NAVIO 3
+#define N 10          // Tamanho do tabuleiro
+#define TAM_NAVIO 3   // Tamanho fixo dos navios
+#define MAR 0         // Representa água
+#define NAVIO 3       // Representa parte do navio
 
-
-int  main(){
+int main() {
     int tab[N][N];
     int i, j;
 
-    // Inicializa o tabuleiro como água (0)
+    // Inicializa o tabuleiro com água
     for (i = 0; i < N; i++)
         for (j = 0; j < N; j++)
-            tab[i][j] = 0;
-            
-
-
-    // Tamanho fixo dos navios
-    int navio[TAM_NAVIO] = {3, 3, 3};
-
-    // Coordenadas iniciais no código
-    int linhaH = 1, colH = 2;  // navio horizontal começa em (1,2)
-    int linV = 4, colV = 7;    // navio vertical começa em (4,7)
+            tab[i][j] = MAR;
 
     int erro = 0;
 
-    // Posicionar navio horizontal
+
+    // 1. Navio horizontal: começa em (1,2)
+    int linhaH = 1, colH = 2;
     if (colH + TAM_NAVIO <= N) {
-        for (i = 0; i < TAM_NAVIO; i++) {
-            if (tab[linhaH][colH + i] != 0) {
+        for (i = 0; i < TAM_NAVIO; i++)
+            if (tab[linhaH][colH + i] != MAR)
                 erro = 1;
-                printf("Erro: sobreposição no navio horizontal.\n");
-            }
-        }
-        if (!erro) {
+
+        if (!erro)
             for (i = 0; i < TAM_NAVIO; i++)
-                tab[linhaH][colH + i] = navio[i];
-        }
+                tab[linhaH][colH + i] = NAVIO;
     } else {
         erro = 1;
-        printf("Erro: navio horizontal fora do tabuleiro.\n");
     }
 
-    // Posicionar navio vertical
-    if (linV + TAM_NAVIO <= N) {
-        for (i = 0; i < TAM_NAVIO; i++) {
-            if (tab[linV + i][colV] != 0) {
+    // 2. Navio vertical: começa em (4,7)
+    int linhaV = 4, colV = 7;
+    if (linhaV + TAM_NAVIO <= N) {
+        for (i = 0; i < TAM_NAVIO; i++)
+            if (tab[linhaV + i][colV] != MAR)
                 erro = 1;
-                printf("Erro: sobreposição no navio vertical.\n");
-            }
-        }
-        if (!erro) {
+
+        if (!erro)
             for (i = 0; i < TAM_NAVIO; i++)
-                tab[linV + i][colV] = navio[i];
-        }
+                tab[linhaV + i][colV] = NAVIO;
     } else {
         erro = 1;
-        printf("Erro: navio vertical fora do tabuleiro.\n");
     }
 
+    // 3. Navio diagonal principal ↘: começa em (0,0)
+    int linD1 = 0, colD1 = 0;
+    if (linD1 + TAM_NAVIO <= N && colD1 + TAM_NAVIO <= N) {
+        for (i = 0; i < TAM_NAVIO; i++)
+            if (tab[linD1 + i][colD1 + i] != MAR)
+                erro = 1;
 
-// Cabeçalho das colunas com letras
-printf("    ");
-for (j = 0; j < N; j++) {
-    printf("%c ", 'A' + j);  // Letras de A até J
-}
-printf("\n");
+        if (!erro)
+            for (i = 0; i < TAM_NAVIO; i++)
+                tab[linD1 + i][colD1 + i] = NAVIO;
+    } else {
+        erro = 1;
+    }
 
-// Impressão do tabuleiro com numeração das linhas
-for (i = 0; i < N; i++) {
-    printf("%2d: ", i + 1);  // Mostra número da linha de 1 a 10
+    // 4. Navio diagonal secundária ↙: começa em (2,7)
+    int linD2 = 2, colD2 = 7;
+    if (linD2 + TAM_NAVIO <= N && colD2 - TAM_NAVIO + 1 >= 0) {
+        for (i = 0; i < TAM_NAVIO; i++)
+            if (tab[linD2 + i][colD2 - i] != MAR)
+                erro = 1;
+
+        if (!erro)
+            for (i = 0; i < TAM_NAVIO; i++)
+                tab[linD2 + i][colD2 - i] = NAVIO;
+    } else {
+        erro = 1;
+    }
+
+    // Cabeçalho com letras
+    printf("    ");
     for (j = 0; j < N; j++) {
-        printf("%d ", tab[i][j]);
+        printf("%c ", 'A' + j);
     }
     printf("\n");
-}
 
-
+    // Imprime cada linha com número à esquerda
+    for (i = 0; i < N; i++) {
+        printf("%2d: ", i + 1);  // Números de 1 a 10
+        for (j = 0; j < N; j++) {
+            printf("%d ", tab[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
